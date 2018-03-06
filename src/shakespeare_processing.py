07525363cd9da1ec3423a565a37aad20912e9f45
Westwood <annechen112995@gmail.com>
2018-03-06 14:20:24 -0800

    Initial data processing: lowercase + remove punctuation

--- src/shakespeare_processing.py
+++ src/shakespeare_processing.py
@@ -1,4 +1,7 @@
-import os, sys
+import nltk
+import string
+
+from collections import Counter
 
 
 def process_data(text):
@@ -9,6 +12,34 @@ def process_data(text):
 
     Output:
     '''
+    new_text = lowercase_no_punctuation(text)
+    tokens = get_tokens(new_text)
+    count = Counter(tokens)
     
+    print(count.most_common(10))
 
-    return text
+    return new_text
+
+def lowercase_no_punctuation(text):
+    '''
+    Convert text to all lowercase and remove punctuation
+    '''
+    new_text = []
+    for line in text:
+        line = line.lower()
+        new_text.append(line.translate(str.maketrans('','',string.punctuation)))
+
+    return new_text
+
+def get_tokens(text):
+    '''
+    Tokenize from text
+    '''
+    token_text = ''
+
+    for line in text:
+        token_text = token_text + line + ' '
+
+    tokens = nltk.word_tokenize(token_text)
+
+    return tokens
