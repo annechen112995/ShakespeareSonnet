bdb2923d6974ec38d51e4237845d246860af6663
Westwood <annechen112995@gmail.com>
2018-03-06 13:01:19 -0800

    Adding .gitignore

---
+++ src/shakespeare_processing.py
@@ -0,0 +1,14 @@
+import os, sys
+
+
+def process_data(text):
+    '''
+    Takes in a text file and tokenizes it.
+
+    Input: Text file, delimited by '\n'.
+
+    Output:
+    '''
+    
+
+    return text
