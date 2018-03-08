0ce985ea6b835ac647907d60b23f4ba974f72dc7
Westwood <annechen112995@gmail.com>
2018-03-08 01:24:34 -0800

    Half implementation

--- src/HMM_helper.py
+++ src/HMM_helper.py
@@ -12,75 +12,7 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from matplotlib import animation
 from matplotlib.animation import FuncAnimation
-
-
-####################
-# WORDCLOUD FUNCTIONS
-####################
-
-def mask():
-    # Parameters.
-    r = 128
-    d = 2 * r + 1
-
-    # Get points in a circle.
-    y, x = np.ogrid[-r:d-r, -r:d-r]
-    circle = (x**2 + y**2 <= r**2)
-
-    # Create mask.
-    mask = 255 * np.ones((d, d), dtype=np.uint8)
-    mask[circle] = 0
-
-    return mask
-
-
-def text_to_wordcloud(text, max_words=50, title='', show=True):
-    plt.close('all')
-
-    # Generate a wordcloud image.
-    wordcloud = WordCloud(random_state=0,
-                          max_words=max_words,
-                          background_color='white',
-                          mask=mask()).generate(text)
-
-    # Show the image.
-    if show:
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis('off')
-        plt.title(title, fontsize=24)
-        plt.show()
-
-    return wordcloud
-
-
-def states_to_wordclouds(hmm, obs_map, max_words=50, show=True):
-    # Initialize.
-    M = 100000
-    n_states = len(hmm.A)
-    obs_map_r = obs_map_reverser(obs_map)
-    wordclouds = []
-
-    # Generate a large emission.
-    emission, states = hmm.generate_emission(M)
-
-    # For each state, get a list of observations that have been emitted
-    # from that state.
-    obs_count = []
-    for i in range(n_states):
-        obs_lst = np.array(emission)[np.where(np.array(states) == i)[0]]
-        obs_count.append(obs_lst)
-
-    # For each state, convert it into a wordcloud.
-    for i in range(n_states):
-        obs_lst = obs_count[i]
-        sentence = [obs_map_r[j] for j in obs_lst]
-        sentence_str = ' '.join(sentence)
-
-        wordclouds.append(text_to_wordcloud(
-            sentence_str, max_words=max_words,
-            title='State %d' % i, show=show))
-
-    return wordclouds
+from dictionaries import import_syllables
 
 
 ####################
@@ -123,7 +55,7 @@ def obs_map_reverser(obs_map):
     return obs_map_r
 
 
-def generate_poem(hmm, obs_map, n_words=100):
+def generate_poem(hmm, obs_map, n_words=500):
     # Get reverse map.
     obs_map_r = obs_map_reverser(obs_map)
 
@@ -131,7 +63,32 @@ def generate_poem(hmm, obs_map, n_words=100):
     emission, states = hmm.generate_emission(n_words)
     sentence = [obs_map_r[i] for i in emission]
 
-    return ' '.join(sentence).capitalize() + '...'
+    # Get dictionary of syllables
+    syllables = import_syllables()
+
+    lines = 0
+    sylls = 0
+    sonnet = ''
+
+    while lines < 14:
+        for word in sentence:
+            if word in syllables.keys():
+                if (len(syllables[word])) > 1:
+                    if 
+                else:
+                    if (sylls + int(syllables[word][0])) < 10:
+                        sonnet += str(word)
+                        sylls += int(syllables[word][0])
+                    elif (sylls + int(syllables[word][0]) == 10):
+                        sonnet = sonnet + str(word) + '\n'
+                        lines += 1
+                        sylls = 0
+                    else:
+                        continue
+            else:
+                continue
+
+    return ' '.join(sonnet).capitalize() + '.'
 
 
 ####################
